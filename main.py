c065689e802abc25f30b1f0c3fb7fe3a69d73d8f
lericraju <lericraju@gmail.com>
2026-02-10 08:40:24 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-"print('main file')" 
+#Calculator
+
+import addition as a
+import subtraction as s
+import multiplication as m
+
+print("addition result=",a.add(5,6))
+print("subtraction result=",s.sub(5,6))
+print("multiplication result=",m.mul(5,6))
